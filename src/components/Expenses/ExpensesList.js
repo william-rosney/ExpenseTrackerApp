4fb57ba1932d40411a925a2d21dea53abefa162c
William Rosney <willia.rosney11@gmail.com>
2022-11-10 11:08:33 -0500

    Learn about React Component, how to share data btw components

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,33 @@
+import Card from "../UI/Card"
+import ExpenseItem from "./ExpenseItem";
+import './ExpensesList.css'
+
+const ExpensesList = (props) => {
+  return (
+    <Card className="expenses">
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}Ω
+          date={props.expenses[1].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
+        ></ExpenseItem> 
+    </Card>
+  );
+}
+
+export default ExpensesList;
+
